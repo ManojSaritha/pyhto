93e26f642bd8fc85d68d1c0ff3463227f6c8b3d4
Manoj P <89677540+ManojSaritha@users.noreply.github.com>
2021-08-28 14:01:53 +0530

    Update Activity_04.py

---
+++ Activity_04.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first value"))
+enter the first value2
+>>> b=int(input("enter the second vaue"))
+enter the second vaue3
+>>> c=a+b
+>>> print (c)
+5
+>>> print('The sum of %d and %d is %d'%(a, b, c))
+The sum of 2 and 3 is 5
+>>> print('%d+%d=%d'%(a,b,c))
+2+3=5
+>>> 
