c3c363faa75ee158de2c0758e0537370fb94c69a
Manoj P <89677540+ManojSaritha@users.noreply.github.com>
2021-08-28 13:36:28 +0530

    Update Activity_02.py

--- Activity_02.py
+++ Activity_02.py
@@ -1,1 +1,8 @@
 #Write your code here:
+>>> string1= input("enter the first string")
+enter the first stringbrqw
+>>> string2=input("enter the second string")
+enter the second stringber
+>>> total=string1+string2
+>>> print(total)
+brqwber
