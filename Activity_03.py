370d5c3bf6684c19b7e9e6ed0df1e40f3cc832c1
Manoj P <89677540+ManojSaritha@users.noreply.github.com>
2021-08-28 13:49:50 +0530

    Update Activity_03.py

---
+++ Activity_03.py
@@ -0,0 +1,14 @@
+string1=input("enter the first string")
+enter the first stringGood
+>>> string2=input("enter the second string")
+enter the second stringMorning
+>>> to=string1+string2
+>>> print(to)
+GoodMorning
+>>> 3*to
+'GoodMorningGoodMorningGoodMorning'
+>>> 5*to
+'GoodMorningGoodMorningGoodMorningGoodMorningGoodMorning'
+>>> print(5*to)
+GoodMorningGoodMorningGoodMorningGoodMorningGoodMorning
+>>> 
